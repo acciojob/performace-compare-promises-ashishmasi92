1e4d55d224e12d904046778a0c00eeb4439d6f49
Ashish <ashishmasih92g@gmail.com>
2025-07-08 01:48:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,37 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
+
+function fetchData (){
+let start = performance.now()
+	Promise.all(apiUrls.map( (v)=>{
+		return fetch(v).then((i)=>i.json())
+	}))
+	.then((res)=>{
+		let end = performance.now()
+		console.log(res)
+		let timeTaken = ((end-start)/1000).toFixed(3)
+		document.getElementById("output-all").innerText = timeTaken
+	})
+
+
+
+	let startany = performance.now()
+Promise.any(apiUrls.map((res)=>{
+	return fetch(res).then((i)=>i.json())
+}))
+	.then((res)=>{
+		let end = performance.now()
+		let timeTaken = ((end-startany)/1000).toFixed(3)
+		document.getElementById("output-any").innerText = timeTaken
+		console.log(res)
+	})
+	
+	
+	
+
+}
+
+fetchData()
+
 // You can write your code here
